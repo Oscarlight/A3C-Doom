9528a44cd20dc1d3fe90a5b7c64b90c23d34e4c6
Oscarlight <oscarli2716@gmail.com>
2017-11-21 14:57:22 -0500

    add run.sh

--- run.sh
+++ run.sh
@@ -1,6 +1,6 @@
-python main.py model0 1000 -thread_num 32
-python main.py model1 1000 -thread_num 16
-python main.py model2 2000 -thread_num 8
-python main.py model3 2000 -thread_num 4
-python main.py model4 4000 -thread_num 2
-python main.py model5 8000 -thread_num 1
+python main.py basic0 1000 -thread_num 32
+python main.py basic1 1000 -thread_num 16
+python main.py basic2 2000 -thread_num 8
+python main.py basic3 2000 -thread_num 4
+python main.py basic4 4000 -thread_num 2
+python main.py basic5 8000 -thread_num 1
